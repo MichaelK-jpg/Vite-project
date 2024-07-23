c96ada157619242c44343aeb2e9961afb7cb63ee
MichaelK-jpg <michalkostrzewski6@gmail.com>
2024-07-23 20:32:04 +0200

    first commit new branch

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,23 @@
 import { useState } from 'react'
 import reactLogo from '../../assets/react.svg'
 import viteLogo from '/vite.svg'
-import './'
 import { FAJNA_ZMIENNA } from '@components/fajny'
 import { Button } from '../Button'
+import { Box } from '../box'
 
-function App() {
+function App({children}: {children: JSX.Element}) {
   const [count, setCount] = useState(0)
+  const [secondCount, setSecondCount] = useState(0)
+  // Opcja 1 - fajna, trochę nieczytelna
+  const [counters, setCounters] = useState<[number, number]>([0, 0])
+  // Opcja 2 - troszkę trudniejsza, bardziej czytelna
+  const [betterCounters, setBetterCounters] = useState({
+    'first': 0,
+    'second': 0,
+  })
+  const handleClick = () => {
+    setSecondCount(secondCount + 1)
+  }; 
 
   return (
     <>
@@ -19,11 +30,14 @@ function App() {
         </a>
       </div>
       <h1>Vite + React {FAJNA_ZMIENNA}</h1>
+      {children}
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={() => setBetterCounters((counters) => ({ ...counters, first: counters.first + 1}))}>
+          count is {betterCounters.first}
         </button>
         <Button label="Test" />
+        <Box count={count} onClick={handleClick}/>
+        
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
